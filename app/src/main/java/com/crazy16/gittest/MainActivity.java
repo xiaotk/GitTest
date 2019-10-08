b2fcf3f2a187c8174e0c77dca701120abe3e4c20
XiaoTk <1611361128@qq.com>
2019-10-08 10:29:59 +0800

    我完成了Hello world 的code

--- app/src/main/java/com/crazy16/gittest/MainActivity.java
+++ app/src/main/java/com/crazy16/gittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.crazy16.gittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
+
     }
 }
